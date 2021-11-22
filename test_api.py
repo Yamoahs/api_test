0739802130228e3b3f106cc14f94159931cb8fb1
Samuel Yamoah <Sam.Yamoah@hotmail.com>
2021-11-22 13:23:01 +1300

    Renamed variable from url to api_url

--- test_api.py
+++ test_api.py
@@ -1,21 +1,21 @@
 import requests
 
 
-def get_api_data(url):
+def get_api_data(api_url):
     """ Function takes an api url and checks that a successful status code is
         return. If success request, the data is returned as a dictionary
         (JSON format)
 
     Args:
-        url (str): api uri to make the GET request to
+        api_url (str): api uri to make the GET request to
 
     Returns:
         dict: Dictionary format of the GET request
     """
     try:
-        response = requests.get(url)
+        response = requests.get(api_url)
         response.raise_for_status()
 
         return response.json()
     except requests.exceptions.HTTPError as error:
-        print(f"Get Request failed: {error}")
+        raise Exception(f"{error}")
