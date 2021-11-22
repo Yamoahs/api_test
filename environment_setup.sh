94a0c34fc7bfe8e0796358d9255b3c97d613cdfc
Samuel Yamoah <Sam.Yamoah@hotmail.com>
2021-11-22 21:16:40 +1300

    Reenabled lines to install python if not already installed
    Python3.8, python3.8-dev tools & python3-pip will be installed if not already

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
 echo "Installing python3.8, python-dev tools, pip and virtual environment"
-#sudo add-apt-repository ppa:deadsnakes/ppa
-#sudo apt-get update
-#sudo apt install python3.8 python3.8-venv
-#sudo apt install python3-pip
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt install python3.8 python3.8-venv
+sudo apt install python3-pip
 
 echo "Creating a Python3.8 Virtual Environment"
 TEST_DIR=$PWD
 VENV_DIR=$TEST_DIR'/test_venv'
 VIRTUAL_ENV=$VENV_DIR'/bin/activate'
 
-python3.8 -m venv "$PWD/test_venv"
+python3.8 -m venv "$VENV_DIR"
 
 echo "Activating Python Virtual Environment"
 # shellcheck disable=SC1090
-source "$PWD"/test_venv/bin/activate
+source "$VIRTUAL_ENV"
 
 echo "Installing required python 3 packages to execute the test"
 pip install -r "$TEST_DIR"/packages.txt
