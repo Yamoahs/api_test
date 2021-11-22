625a4ef911c3813b0656c1b68a1a42cef65a6dcb
Samuel Yamoah <Sam.Yamoah@hotmail.com>
2021-11-22 13:56:54 +1300

    Added verify_name test
    Added a test case that verifies the name field from the api request matches the expected name

--- test_api.py
+++ test_api.py
@@ -1,5 +1,9 @@
 import requests
 
+# API URL under test
+url = ("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?"
+       "catalogue=false")
+
 
 def get_api_data(api_url):
     """ Function takes an api url and checks that a successful status code is
@@ -19,3 +23,20 @@ def get_api_data(api_url):
         return response.json()
     except requests.exceptions.HTTPError as error:
         raise Exception(f"{error}")
+
+
+def test_verify_name():
+    """ Test Checks the Name field from the api data is as expected
+
+    Returns:
+        None
+    """
+    print(f"Test verifies name returned is expected name")
+    expected_name = "Carbon credits"
+    api_data = get_api_data(url)
+    name_returned = api_data.get("Name")
+
+    print(f"Expected name: {expected_name}, name returned "
+          f"from api: {name_returned}")
+    assert name_returned == expected_name
+
